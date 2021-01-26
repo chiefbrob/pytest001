cb06bded7d13cf3558e7c3f687c2e53a1f9c18a7
Brian Chief Obare <brian.obare@pfizer.com>
2021-01-26 09:58:03 +0300

    setup virtual environment

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import sys
+import requests
 
 print(sys.version)
 print(sys.executable)
 
+
 def greeting(who):
-  return 'Hello, {}'.format(who)
+    return 'Hello, {}'.format(who)
+
 
 print(greeting('Chief'))
+
+r = requests.get('https://chiefbrob.info')
+print(r.status_code)
